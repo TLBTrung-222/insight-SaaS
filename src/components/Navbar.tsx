866bd8fc748d49b3581492940581d1e7007eff3e
TrungTLB <baotrung2852003@gmail.com>
2023-11-02 18:46:14 +0700

    Creating the nav bar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,51 @@
+import Link from "next/link";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import { buttonVariants } from "./ui/button";
+import { RegisterLink, LoginLink } from "@kinde-oss/kinde-auth-nextjs/server";
+
+const Navbar = () => {
+    return (
+        <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
+            <MaxWidthWrapper>
+                <div className="flex h-14 items-center justify-between border-b border-zinc-200">
+                    {/* Logo (left) */}
+                    <Link href="/" className="flex z-40 font-semibold">
+                        <span>Insights.</span>
+                    </Link>
+
+                    {/* Button to navigate */}
+                    <div className="hidden items-center space-x-4 sm:flex">
+                        <>
+                            <Link
+                                href="/pricing"
+                                className={buttonVariants({
+                                    variant: "ghost",
+                                    size: "sm",
+                                })}
+                            >
+                                Pricing
+                            </Link>
+                            <LoginLink
+                                className={buttonVariants({
+                                    variant: "ghost",
+                                    size: "sm",
+                                })}
+                            >
+                                Sign in
+                            </LoginLink>
+                            <RegisterLink
+                                className={buttonVariants({
+                                    size: "sm",
+                                })}
+                            >
+                                Sign up
+                            </RegisterLink>
+                        </>
+                    </div>
+                </div>
+            </MaxWidthWrapper>
+        </nav>
+    );
+};
+
+export default Navbar;
