0d40c42e85bb63df6f0a1538d66ca792c12d5afd
TrungTLB <baotrung2852003@gmail.com>
2023-11-04 19:16:51 +0700

    setup tRPC

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,7 +1,5 @@
-"use client";
-
 import { useRouter, useSearchParams } from "next/navigation";
-import React from "react";
+import { trpc } from "../_trpc/client";
 
 export default function Page() {
     // sync the logged in user to database
@@ -9,6 +7,15 @@ export default function Page() {
     // get the page user is standing
     const searchParams = useSearchParams();
     const origin = searchParams.get("origin"); // origin = "dashboard"
+
+    trpc.authCallback.useQuery(undefined, {
+        onSuccess: ({ success }) => {
+            if (success) {
+                // user is synced to db -> redirect user to dashboard
+                router.push(origin ? `/${origin}` : "/dashboard");
+            }
+        },
+    }); // undefined cuz this is query, not mutation like post request...
 
     return <div>hello</div>;
 }
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,6 +1,21 @@
-import { router } from "./trpc";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { publicProcedure, router } from "./trpc";
+import { TRPCError } from "@trpc/server";
 export const appRouter = router({
     // define our backend API logic here
+    authCallback: publicProcedure.query(() => {
+        const { getUser } = getKindeServerSession();
+        const user = getUser();
+
+        // if user not exist, throw error
+        if (!user.id || !user.email)
+            throw new TRPCError({ code: "UNAUTHORIZED" });
+
+        // check if the user already in database?
+        // we will have database later...
+
+        return { success: true };
+    }),
 });
 
 // Export type router type signature,
